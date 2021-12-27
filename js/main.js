33ec1a83da2cd2ecd23e185fdfc330ebbcf126b9
Maia Francine Endaya <maia.endaya@gmail.com>
2021-12-27 13:49:10 +0800

    sphere_added_2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//insert code here
-const sphereGeometry = new THREE.sphereGeometry(8.613, 32, 16)
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xf5e942 });
+
+//Sphere Geometry
+const sphereGeometry = new THREE.SphereGeometry(5, 32, 16);
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x347deb });
 const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 scene.add(sphere);
 
-camera.position.z = 1;
+camera.position.z = 35;
 
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
-  //end
+  sphere.position.x += 0.09;
+  sphere.position.y += 0.09;
+
   renderer.render(scene, camera);
 }
 animate();
