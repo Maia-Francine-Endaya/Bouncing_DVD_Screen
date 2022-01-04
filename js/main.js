dad752d7659cc66bc0b2faffd68b32d24ce928c7
Maia Francine Endaya <maia.endaya@gmail.com>
2022-01-04 10:16:58 +0800

    changed_shape

--- js/main.js
+++ js/main.js
@@ -1,24 +1,22 @@
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(65, 800 / 800, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
-//Sphere Geometry
-const sphereGeometry = new THREE.SphereGeometry(5, 32, 16);
-const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0x347deb });
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphere);
+const planeGeometry = new THREE.PlaneGeometry(10, 10);
+const planeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
 
 camera.position.z = 35;
 
 function animate() {
   requestAnimationFrame(animate);
 
-  sphere.position.x += 0.09;
-  sphere.position.y += 0.09;
+  plane.position.x += 0.07;
+  plane.position.y += 0.07;
 
   renderer.render(scene, camera);
 }
